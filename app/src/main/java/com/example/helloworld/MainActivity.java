7570f4b15359f9cbe1b1dafa0bf57721c6e392c1
Arya Bhattacharyya <70765320+xxEasterGrymm@users.noreply.github.com>
2021-10-08 11:19:48 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
     public void onBtnClick(View view) {
         TextView firstName = findViewById(R.id.txtFirstName);
         EditText edtFirstName = findViewById(R.id.edtFirstName);
